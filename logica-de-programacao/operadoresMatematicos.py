5170c847c0edcc3c040066ea467021fe56cc8b5a
Rodrigo Marçal <rodrigo.m.ribeiro1@proton.me>
2023-11-19 03:35:21 -0300

    Adicionar arquivo da aula 1: operadores matemáticos

---
+++ logica-de-programacao/operadoresMatematicos.py
@@ -0,0 +1,22 @@
+# Operadores matemáticos
+
+valor1 = 10
+valor2 = 5
+
+# adição (+)
+# subtração (-)
+# multiplicação (*)
+# divisão (/)
+# resto da divisão (%) 
+
+soma = valor1 + valor2 
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+restoDaDivisao = valor1 % valor2
+
+print("A soma de {} e {} é igual a {}".format(valor1, valor2, soma))
+print("A diferença entre {} e {} é igual a {}".format(valor1, valor2, subtracao))
+print("A multiplicação de {} por {} é igual a {}".format(valor1, valor2, multiplicacao))
+print("A divisão entre {} e {} é igual: {}".format(valor1, valor2, divisao))
+print("O resto da divisão entre {} e {} é {}".format(valor1, valor2, restoDaDivisao))
