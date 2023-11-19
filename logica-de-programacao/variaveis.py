ab298fa2f0a40e6c794ecac1a95d8832e6680ce0
Rodrigo Marçal <rodrigo.m.ribeiro1@proton.me>
2023-11-19 03:21:46 -0300

    Adicionar arquivo da aula 1: variáveis

---
+++ logica-de-programacao/variaveis.py
@@ -0,0 +1,6 @@
+nome = "Rodrigo" # String (str)
+idade = 20 # número inteiro (int)
+altura = 1.82 # número com ponto flutuante (float)
+estudante = True # booleano (bool)
+
+print("nome:", nome, "\nidade:", idade, "anos", "\naltura:", altura, "metros")
