64340c890ebec54cb3ea805e8589b49c3318d277
Rodrigo Marçal <rodrigo.m.ribeiro1@proton.me>
2023-11-23 19:00:52 -0300

    Adicionar arquivo aula 2: controle de fluxo

---
+++ logica-de-programacao/controleDeFluxo.py
@@ -0,0 +1,15 @@
+idade = 13
+
+
+if idade >= 18:
+    maiorDeIdade = True
+else:
+    maiorDeIdade = False
+
+if maiorDeIdade:
+    print("Parabéns, você é maior de idade")
+else:
+    print("Que peninha, você é de menor :(")
+
+
+
