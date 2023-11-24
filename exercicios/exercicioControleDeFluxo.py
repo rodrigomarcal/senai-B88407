a970f95870fe60d56b38b984f23fffc7e0d6dddc
Rodrigo Marçal <rodrigo.m.ribeiro1@proton.me>
2023-11-24 19:13:12 -0300

    Remover condição e alterar resultado da condicional

--- exercicios/exercicioControleDeFluxo.py
+++ exercicios/exercicioControleDeFluxo.py
@@ -6,11 +6,9 @@ print("Sua idade é:",idade)
 
 if idade >= 60:
     print("Você é um idoso")
-elif idade > 18:
+elif idade >= 18:
     print("Você é adulto")
-elif idade >= 12:
+elif idade > 12:
     print("Você é um adolescente")
-elif idade > 2:
-    print("Você é uma criança")
 else:
-    print("Você é um bebê")
+    print("Você é uma criança")
